6a3392f78f30b8a6af5f2a0544e9c428e98739c2
NicolaiVBonderup <nvbonderup@gmail.com>
2017-10-10 18:09:32 +0200

    Added a method for retrieving a single post from DB by ID.+

--- app/Hackernews/Access/CommentAccess.php
+++ app/Hackernews/Access/CommentAccess.php
@@ -123,6 +123,71 @@ class CommentAccess implements ICommentAccess
         }
     }
 
+    /**
+     * @param int $commentId
+     * @return Comment
+     */
+    public function getCommentById(int $commentId)
+    {
+        try {
+            $stmt = DB::conn()->prepare("
+                SELECT c.id   AS comment_id, 
+                c.user_ref    AS comment_user_ref, 
+                c.post_ref    AS comment_post_ref, 
+                c.comment_ref AS comment_comment_ref, 
+                c.content     AS comment_content, 
+                c.karma       AS comment_karma, 
+                c.spam        AS comment_spam, 
+                c.created_at  AS comment_created_at,
+                u.id          AS user_id, 
+                u.karma       AS user_karma, 
+                u.alias       AS user_alias  
+                FROM comments c
+                JOIN users u ON c.user_ref = u.id
+                WHERE c.id = :comment_id
+            ");
+
+            $stmt->execute([
+                'comment_id' => $commentId
+            ]);
+
+            $comment = null;
+
+            while ($row = $stmt->fetch()) {
+                $user = new User(
+                    $row['user_id'],
+                    $row['user_karma'],
+                    $row['user_alias']
+                );
+
+                $comment = new Comment(
+                    $row['comment_id'],
+                    $row['comment_user_ref'],
+                    $row['comment_post_ref'],
+                    $row['comment_comment_ref'],
+                    $row['comment_content'],
+                    $row['comment_karma'],
+                    $row['comment_spam'],
+                    $row['comment_created_at']
+                );
+
+            }
+
+            return $comment
+
+        } catch (PDOException £e) {
+
+        }
+    }
+
+
+    /**
+     * @param int $userRef
+     * @param int $postRef
+     * @param int $commentRef
+     * @param string $content
+     * @return array
+     */
     public function postCommentWithReference(int $userRef, int $postRef, int $commentRef, string $content)
     {
         try {
